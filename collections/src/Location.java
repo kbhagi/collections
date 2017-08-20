1050c285072e57d002670a130634b3ffeb2845b8
bhargavak <bhargavak37@gmail.com>
2017-08-20 13:17:38 +0530

    Collections Interface in Java
    Collections Interface in Java, methods on collections interfaces

---
+++ collections/src/Location.java
@@ -0,0 +1,31 @@
+package bhargavak;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+	private final int locationID;
+	private final String description;
+	private final Map<String, Integer> exits;
+
+	public Location(int locationID, String description) {
+		this.locationID = locationID;
+		this.description = description;
+		this.exits = new HashMap<String, Integer>();
+	}
+
+	public int getLocationID() {
+		return locationID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Map<String, Integer> getExits() {
+		return exits;
+	}
+
+
+
+}
